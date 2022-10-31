df6377bf3b11e00ce193d733c6fac8820756a336
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2022-10-31 16:18:41 -0500

    Initial Commit

---
+++ GaugesDemo/GaugesDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GaugesDemoApp.swift
+//  GaugesDemo
+//
+//  Created by Mark Volkmann on 10/31/22.
+//
+
+import SwiftUI
+
+@main
+struct GaugesDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
